3eedec1708879e00e1a4c95527e83c694097d491
thanhnguyen313 <tothanhnguyen2006@gmail.com>
2025-03-29 15:12:27 +0700

    update time

---
+++ TIME/TIME/TIME.cpp
@@ -0,0 +1,12 @@
+// TIME.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "TIME.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
